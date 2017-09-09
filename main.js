ffcaaee48372d25f38ac3c2a6381df4201b36a3d
Devine Lu Linvega <aliceffekt@gmail.com>
2017-09-10 09:07:42 +1300

    Added OS X shortcuts for hide/show and Minimize.

--- main.js
+++ main.js
@@ -2,26 +2,19 @@ const {app, BrowserWindow, webFrame, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-
-
+app.on('ready', () => 
+{
   if (process.platform === 'darwin') {
-    // Create the browser window.
     win = new BrowserWindow({width: 1100, height: 660, frame:false, backgroundColor: 'rgba(0,0,0,0.1)', show:false,  resizable:true, transparent: true, autoHideMenuBar: true, icon: __dirname + '/icon.ico'})
 
     var nativeHandleBuffer = win.getNativeWindowHandle();
 
     win.loadURL(`file://${__dirname}/sources/index.html`)
+      
+    let is_shown = true;
 
-    // Create our menu entries so that we can use MAC shortcuts
     Menu.setApplicationMenu(Menu.buildFromTemplate([
       {
         label: 'Edit',
@@ -33,61 +26,53 @@ app.on('ready', () => {
           { role: 'paste' },
           { role: 'delete' },
           { role: 'selectall' },
-          {label: 'Quit', accelerator: 'CmdOrCtrl+Q', click: function() { force_quit=true; app.exit();}},
+          { label: 'Hide', accelerator: 'CmdOrCtrl+H',click: () => { if(is_shown){ win.hide(); } else{ win.show(); }}},
+          { label: 'Minimize', accelerator: 'CmdOrCtrl+M',click: () => { win.minimize(); }},
+          { label: 'Quit', accelerator: 'CmdOrCtrl+Q', click: function() { force_quit=true; app.exit(); }},
         ]
       }
     ]));
 
     win.on('ready-to-show',function() {
-
       win.show();
-
     })
 
-  }
+    win.on('hide',function() {
+      is_shown = false;
+    })
 
+    win.on('show',function() {
+      is_shown = true;
+    })
+  }
   else {
-    // Create the browser window.
     win = new BrowserWindow({width: 1100, height: 660, frame:false, transparent: true, backgroundColor: 'rgba(200,200,200,.7)', show:false, resizable:true, autoHideMenuBar: true, icon: __dirname + '/icon.ico'})
 
     win.loadURL(`file://${__dirname}/sources/index.html`)
 
     var nativeHandleBuffer = win.getNativeWindowHandle();
+
     win.on('ready-to-show',function() {
-
       win.show();
-
     })
 
-    // Emitted when the window is closed.
     win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
       win = null
       app.quit()
     })
   }
-
-
   // Open the DevTools.
   // win.webContents.openDevTools()
 })
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+app.on('window-all-closed', () => 
+{
   app.quit()
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+app.on('activate', () => 
+{
   if (win === null) {
     createWindow()
-  }
-  else{
-
   }
 })
