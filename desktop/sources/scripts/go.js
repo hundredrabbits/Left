17928a9953963d4e234b25d43bd711e7cba6ad74
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-06 06:39:30 +1200

    Turns out the method was unused after all.

--- desktop/sources/scripts/go.js
+++ desktop/sources/scripts/go.js
@@ -53,45 +53,6 @@ function Go () {
     this.to(next, next, scroll)
   }
 
-  this.to_word = function (word, from = 0, tries = 0, startingWith = false, endingWith = false) {
-    let target = word
-
-    if (startingWith) { target = target.substr(0, target.length - 1) }
-    if (endingWith) { target = target.substr(1, target.length - 1) }
-
-    // TODO: fix length reference here: not sure what it is supposed to be
-    if (left.textarea_el.value.substr(from, length).indexOf(target) === -1 || tries < 1) { console.log('failed'); return }
-
-    const length = left.textarea_el.value.length - from
-    const segment = left.textarea_el.value.substr(from, length)
-    const location = segment.indexOf(target)
-    const charBefore = segment.substr(location - 1, 1)
-    const charAfter = segment.substr(location + target.length, 1)
-
-    // Check for full word
-    if (!startingWith && !endingWith && !charBefore.match(/[a-z]/i) && !charAfter.match(/[a-z]/i)) {
-      left.select(location + from, location + from + target.length)
-      const perc = (left.textarea_el.selectionEnd / parseFloat(left.chars_count))
-      const offset = 60
-      left.textarea_el.scrollTop = (left.textarea_el.scrollHeight * perc) - offset
-      return location
-    } else if (startingWith && !charBefore.match(/[a-z]/i) && charAfter.match(/[a-z]/i)) {
-      left.select(location + from, location + from + target.length)
-      const perc = (left.textarea_el.selectionEnd / parseFloat(left.chars_count))
-      const offset = 60
-      left.textarea_el.scrollTop = (left.textarea_el.scrollHeight * perc) - offset
-      return location
-    } else if (endingWith && charBefore.match(/[a-z]/i) && !charAfter.match(/[a-z]/i)) {
-      left.select(location + from, location + from + target.length)
-      const perc = (left.textarea_el.selectionEnd / parseFloat(left.chars_count))
-      const offset = 60
-      left.textarea_el.scrollTop = (left.textarea_el.scrollHeight * perc) - offset
-      return location
-    }
-
-    this.to_word(word, location + target.length, tries - 1, startingWith, endingWith)
-  }
-
   this.scroll_to = function (from, to) {
     const textVal = left.textarea_el.value
     const div = document.createElement('div')
