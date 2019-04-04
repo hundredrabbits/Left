fecb5df28129244dae293d8dad16af3060f51f89
Devine Lu Linvega <aliceffekt@gmail.com>
2019-04-05 08:48:34 +0900

    Fixes #70

--- desktop/main.js
+++ desktop/main.js
@@ -1,88 +1,76 @@
-const { app, Menu, BrowserWindow } = require('electron')
+const { app, BrowserWindow, webFrame, Menu } = require('electron')
 const path = require('path')
+const url = require('url')
+const shell = require('electron').shell
 
 let isShown = true
 
-function createWindow () {
-  app.win = new BrowserWindow({
-    width: 880,
-    height: 530,
-    backgroundColor: '#000',
-    minWidth: 587,
-    minHeight: 540,
-    frame: false,
-    autoHideMenuBar: true,
-    icon: path.join(__dirname, '/icon.ico')
-  })
+app.win = null
 
-  const { webContents } = app.win
-  webContents.on('did-finish-load', () => {
-    webContents.setZoomFactor(1)
-    webContents.setVisualZoomLevelLimits(1, 1)
-    webContents.setLayoutZoomLevelLimits(0, 0)
+app.on('ready', () => {
+  app.win = new BrowserWindow({
+    width: 800,
+    height: 530,
+    minWidth: 310,
+    minHeight: 350,
+    backgroundColor: '#000',
+    icon: __dirname + '/' + { darwin: 'icon.icns', linux: 'icon.png', win32: 'icon.ico' }[process.platform] || 'icon.ico',
+    resizable: true,
+    frame: process.platform !== 'darwin',
+    skipTaskbar: process.platform === 'darwin',
+    autoHideMenuBar: process.platform === 'darwin',
+    webPreferences: { zoomFactor: 1.0 },
+    webPreferences: { backgroundThrottling: false }
   })
 
   app.win.loadURL(`file://${__dirname}/sources/index.html`)
 
-  // Uncomment to show devTools on load
-  // app.inspect();
-
   app.win.on('closed', () => {
-    app.win = null
+    win = null
     app.quit()
   })
 
-  app.win.on('hide', () => {
+  app.win.on('hide', function () {
     isShown = false
   })
 
-  app.win.on('show', () => {
+  app.win.on('show', function () {
     isShown = true
   })
-}
 
-app.on('ready', () => {
-  createWindow()
+  app.on('window-all-closed', () => {
+    app.quit()
+  })
+
+  app.on('activate', () => {
+    if (app.win === null) {
+      createWindow()
+    } else {
+      app.win.show()
+    }
+  })
 })
-
-app.on('window-all-closed', () => {
-  app.quit()
-})
-
-app.on('activate', () => {
-  if (app.win === null) {
-    createWindow()
-  } else {
-    app.win.show()
-  }
-})
-
-// Helpers
 
 app.inspect = function () {
   app.win.toggleDevTools()
 }
 
-app.toggle_fullscreen = function () {
+app.toggleFullscreen = function () {
   app.win.setFullScreen(!app.win.isFullScreen())
 }
 
-app.toggle_visible = function () {
-  if (process.platform === 'win32') {
-    if (!app.win.isMinimized()) { app.win.minimize() } else { app.win.restore() }
-  } else {
+app.toggleVisible = function () {
+  if (process.platform === 'darwin') {
     if (isShown && !app.win.isFullScreen()) { app.win.hide() } else { app.win.show() }
+  } else {
+    if (!app.win.isMinimized()) { app.win.minimize() } else { app.win.restore() }
   }
 }
 
-app.inject_menu = function (menu) {
+app.injectMenu = function (menu) {
   try {
     Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
   } catch (err) {
     console.warn('Cannot inject menu.')
   }
-}
-
-app.path = function () {
-  return __dirname
 }
