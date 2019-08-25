488b4c99cd4add7592f71f728ff664c8fbc803c3
Stuart Kent <stuart@stuartkent.co.nz>
2019-08-25 20:19:27 +1200

    Added way for windows users to disable the frame with a shortcut arg of "frameless"

--- desktop/main.js
+++ desktop/main.js
@@ -16,7 +16,7 @@ app.on('ready', () => {
     backgroundColor: '#000',
     icon: path.join(__dirname, { darwin: 'icon.icns', linux: 'icon.png', win32: 'icon.ico' }[process.platform] || 'icon.ico'),
     resizable: true,
-    frame: process.platform !== 'darwin',
+    frame: (process.argv.indexOf('frameless') == -1) && (process.platform !== 'darwin'),
     skipTaskbar: process.platform === 'darwin',
     autoHideMenuBar: process.platform === 'darwin',
     webPreferences: { zoomFactor: 1.0, nodeIntegration: true, backgroundThrottling: false }
