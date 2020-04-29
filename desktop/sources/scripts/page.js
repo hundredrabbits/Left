d86a744e3b0f3f6c444faa4ab0726d64859ffb44
dethe <dethe@livingcode.org>
2020-04-29 11:56:33 -0700

    add two more levels of subheading

--- desktop/sources/scripts/page.js
+++ desktop/sources/scripts/page.js
@@ -1,60 +1,103 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const EOL = '\n'
+const fs = require("fs");
+const EOL = "\n";
 
-function Page (text = '', path = null) {
-  this.text = text.replace(/\r?\n/g, '\n')
-  this.path = path
+function Page(text = "", path = null) {
+  this.text = text.replace(/\r?\n/g, "\n");
+  this.path = path;
 
   this.name = function () {
-    if (!this.path) { return 'Untitled' }
+    if (!this.path) {
+      return "Untitled";
+    }
 
-    const parts = this.path.replace(/\\/g, '/').split('/')
-    return parts[parts.length - 1]
-  }
+    const parts = this.path.replace(/\\/g, "/").split("/");
+    return parts[parts.length - 1];
+  };
 
   this.has_changes = function () {
     if (!this.path) {
-      if (this.text && this.text.length > 0) { return true }
-      return false
+      if (this.text && this.text.length > 0) {
+        return true;
+      }
+      return false;
     }
-    return this.load() !== this.text
-  }
+    return this.load() !== this.text;
+  };
 
   this.commit = function (text = left.textarea_el.value) {
-    this.text = text
-  }
+    this.text = text;
+  };
 
   this.reload = function (force = false) {
-    if (!this.path) { return }
+    if (!this.path) {
+      return;
+    }
 
     if (!this.has_changes() || force) {
-      this.commit(this.load())
+      this.commit(this.load());
     }
-  }
+  };
 
   this.load = function () {
-    if (!this.path) { return }
-    let data
-    try {
-      data = fs.readFileSync(this.path, 'utf-8')
-    } catch (err) {
-      this.path = null
-      return
+    if (!this.path) {
+      return;
     }
-    return data
-  }
+    let data;
+    try {
+      data = fs.readFileSync(this.path, "utf-8");
+    } catch (err) {
+      this.path = null;
+      return;
+    }
+    return data;
+  };
 
   this.markers = function () {
-    const a = []
-    const lines = this.text.split(EOL)
+    const a = [];
+    const lines = this.text.split(EOL);
     for (const id in lines) {
-      const line = lines[id].trim()
-      if (line.substr(0, 2) === '##') { a.push({ id: a.length, text: line.replace('##', '').trim(), line: parseInt(id), type: 'subheader' }) } else if (line.substr(0, 1) === '#') { a.push({ id: a.length, text: line.replace('#', '').trim(), line: parseInt(id), type: 'header' }) } else if (line.substr(0, 2) === '--') { a.push({ id: a.length, text: line.replace('--', '').trim(), line: parseInt(id), type: 'comment' }) }
+      const line = lines[id].trim();
+      if (line.substr(0, 4) === "####") {
+        a.push({
+          id: a.length,
+          text: line.replace("####", "").trim(),
+          line: parseInt(id),
+          type: "header4",
+        });
+      } else if (line.substr(0, 3) === "###") {
+        a.push({
+          id: a.length,
+          text: line.replace("###", "").trim(),
+          line: parseInt(id),
+          type: "header3",
+        });
+      } else if (line.substr(0, 2) === "##") {
+        a.push({
+          id: a.length,
+          text: line.replace("##", "").trim(),
+          line: parseInt(id),
+          type: "subheader",
+        });
+      } else if (line.substr(0, 1) === "#") {
+        a.push({
+          id: a.length,
+          text: line.replace("#", "").trim(),
+          line: parseInt(id),
+          type: "header",
+        });
+      } else if (line.substr(0, 2) === "--") {
+        a.push({
+          id: a.length,
+          text: line.replace("--", "").trim(),
+          line: parseInt(id),
+          type: "comment",
+        });
+      }
     }
-    return a
-  }
+    return a;
+  };
 }
 
-module.exports = Page
+module.exports = Page;
