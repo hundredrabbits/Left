a9e0473c89daa4712ff1400e24ee562b8598248b
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2021-11-16 19:34:09 +0100

    added load function

--- desktop/sources/scripts/oak.js
+++ desktop/sources/scripts/oak.js
@@ -12,6 +12,15 @@ function Oak() {
     });
     return themes;
   };
+  this.load = (name) => {
+    console.log("Oak", `Loading Theme ${name}..`);
+    const fs = require("fs");
+    const themePath = `${__dirname}/../media/themes/${name}.svg`;
+    const themeExists = fs.existsSync(themePath);
+    if (themeExists) {
+      const theme = fs.readFileSync(themePath, "utf-8");
+    }
+  };
 }
 
 module.exports = Oak;
