bb896458ea5ce5ac610429c732b3dad20d4f76e0
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2021-11-18 09:53:44 +0100

    added branch element with constructor

--- desktop/sources/scripts/branch.js
+++ desktop/sources/scripts/branch.js
@@ -2,6 +2,11 @@
 
 function Branch() {
   this.menu = {};
+  this.branch_el;
+
+  this.start = () => {
+    this.branch_el = document.createElement("div");
+  };
 
   this.setMenu = (menu) => {
     this.menu = menu;
