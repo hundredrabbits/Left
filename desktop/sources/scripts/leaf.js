1b0bd28533c46bcd71e8d67db7edb30e2b49de21
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2021-11-18 11:36:08 +0100

    added leaf: adds tray with contextmenu

--- desktop/sources/scripts/leaf.js
+++ desktop/sources/scripts/leaf.js
@@ -1,7 +1,49 @@
 "use strict";
 
-const { app, Menu } = require("electron").remote;
+const { app, Menu, Tray } = require("electron").remote;
 
-function Leaf() {}
+function Leaf() {
+  this.menuTemplate = {};
+  this.contextMenu;
+  this.tray;
+
+  this.start = () => {
+    this.tray = new Tray(`${__dirname}/../../icon.ico`);
+    this.inject();
+  };
+
+  this.setMenu = (menu) => {
+    this.menuTemplate = menu;
+  };
+
+  this.inject = () => {
+    this.contextMenu = Menu.buildFromTemplate(this.format());
+    this.tray.setContextMenu(this.contextMenu);
+  };
+
+  this.format = function () {
+    const f = [];
+    const m = this.menuTemplate;
+    for (const cat in m) {
+      const submenu = [];
+      for (const name in m[cat]) {
+        const option = m[cat][name];
+        if (option.role) {
+          submenu.push({ role: option.role });
+        } else if (option.type) {
+          submenu.push({ type: option.type });
+        } else {
+          submenu.push({
+            label: name,
+            accelerator: option.accelerator,
+            click: option.fn,
+          });
+        }
+      }
+      f.push({ label: cat, submenu: submenu });
+    }
+    return f;
+  };
+}
 
 module.exports = Leaf;
