7473ed5b4ad0c95a239853b0cf0d62408fa7c175
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2021-11-18 09:12:46 +0100

    changed clarity of console log

--- desktop/sources/scripts/lib/controller.js
+++ desktop/sources/scripts/lib/controller.js
@@ -1,86 +1,121 @@
-'use strict'
+"use strict";
 
-function Controller () {
-  const fs = require('fs')
-  const { dialog, app } = require('electron').remote
+function Controller() {
+  const fs = require("fs");
+  const { dialog, app } = require("electron").remote;
 
-  this.menu = { default: {} }
-  this.mode = 'default'
+  this.menu = { default: {} };
+  this.mode = "default";
 
-  this.app = require('electron').remote.app
+  this.app = require("electron").remote.app;
 
-  this.start = function () {
-  }
+  this.start = function () {};
 
   this.add = function (mode, cat, label, fn, accelerator) {
-    if (!this.menu[mode]) { this.menu[mode] = {} }
-    if (!this.menu[mode][cat]) { this.menu[mode][cat] = {} }
-    this.menu[mode][cat][label] = { fn: fn, accelerator: accelerator }
-  }
+    if (!this.menu[mode]) {
+      this.menu[mode] = {};
+    }
+    if (!this.menu[mode][cat]) {
+      this.menu[mode][cat] = {};
+    }
+    this.menu[mode][cat][label] = { fn: fn, accelerator: accelerator };
+  };
 
   this.addRole = function (mode, cat, label) {
-    if (!this.menu[mode]) { this.menu[mode] = {} }
-    if (!this.menu[mode][cat]) { this.menu[mode][cat] = {} }
-    this.menu[mode][cat][label] = { role: label }
-  }
+    if (!this.menu[mode]) {
+      this.menu[mode] = {};
+    }
+    if (!this.menu[mode][cat]) {
+      this.menu[mode][cat] = {};
+    }
+    this.menu[mode][cat][label] = { role: label };
+  };
 
-  this.addSpacer = function (mode, cat, label, type = 'separator') {
-    if (!this.menu[mode]) { this.menu[mode] = {} }
-    if (!this.menu[mode][cat]) { this.menu[mode][cat] = {} }
-    this.menu[mode][cat][label] = { type: type }
-  }
+  this.addSpacer = function (mode, cat, label, type = "separator") {
+    if (!this.menu[mode]) {
+      this.menu[mode] = {};
+    }
+    if (!this.menu[mode][cat]) {
+      this.menu[mode][cat] = {};
+    }
+    this.menu[mode][cat][label] = { type: type };
+  };
 
   this.clearCat = function (mode, cat) {
-    if (this.menu[mode]) { this.menu[mode][cat] = {} }
-  }
+    if (this.menu[mode]) {
+      this.menu[mode][cat] = {};
+    }
+  };
 
-  this.set = function (mode = 'default') {
-    this.mode = mode
-    this.commit()
-  }
+  this.set = function (mode = "default") {
+    this.mode = mode;
+    this.commit();
+  };
 
   this.format = function () {
-    const f = []
-    const m = this.menu[this.mode]
+    const f = [];
+    const m = this.menu[this.mode];
     for (const cat in m) {
-      const submenu = []
+      const submenu = [];
       for (const name in m[cat]) {
-        const option = m[cat][name]
+        const option = m[cat][name];
         if (option.role) {
-          submenu.push({ role: option.role })
+          submenu.push({ role: option.role });
         } else if (option.type) {
-          submenu.push({ type: option.type })
+          submenu.push({ type: option.type });
         } else {
-          submenu.push({ label: name, accelerator: option.accelerator, click: option.fn })
+          submenu.push({
+            label: name,
+            accelerator: option.accelerator,
+            click: option.fn,
+          });
         }
       }
-      f.push({ label: cat, submenu: submenu })
+      f.push({ label: cat, submenu: submenu });
     }
-    return f
-  }
+    return f;
+  };
 
   this.commit = function () {
-    console.log('Controller', 'Changing..')
-    this.app.injectMenu(this.format())
-  }
+    console.log("Controller", "Committing..");
+    this.app.injectMenu(this.format());
+  };
 
   this.accelerator = function (key, menu) {
-    const acc = { basic: null, ctrl: null }
+    const acc = { basic: null, ctrl: null };
     for (cat in menu) {
-      const options = menu[cat]
+      const options = menu[cat];
       for (const id in options.submenu) {
-        const option = options.submenu[id]; if (option.role) { continue }
-        acc.basic = (option.accelerator.toLowerCase() === key.toLowerCase()) ? option.label.toUpperCase().replace('TOGGLE ', '').substr(0, 8).trim() : acc.basic
-        acc.ctrl = (option.accelerator.toLowerCase() === ('CmdOrCtrl+' + key).toLowerCase()) ? option.label.toUpperCase().replace('TOGGLE ', '').substr(0, 8).trim() : acc.ctrl
+        const option = options.submenu[id];
+        if (option.role) {
+          continue;
+        }
+        acc.basic =
+          option.accelerator.toLowerCase() === key.toLowerCase()
+            ? option.label
+                .toUpperCase()
+                .replace("TOGGLE ", "")
+                .substr(0, 8)
+                .trim()
+            : acc.basic;
+        acc.ctrl =
+          option.accelerator.toLowerCase() ===
+          ("CmdOrCtrl+" + key).toLowerCase()
+            ? option.label
+                .toUpperCase()
+                .replace("TOGGLE ", "")
+                .substr(0, 8)
+                .trim()
+            : acc.ctrl;
       }
     }
-    return acc
-  }
+    return acc;
+  };
 
   this.docs = function () {
     // TODO
-    console.log(this.menu.default)
-  }
+    console.log(this.menu.default);
+  };
 }
 
-module.exports = Controller
+module.exports = Controller;
